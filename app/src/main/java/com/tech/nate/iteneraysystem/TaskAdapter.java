c7c807fe38ea7347322b195957586a2f1911132d
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-20 04:10:12 +0000

    Adapter Created

---
+++ app/src/main/java/com/tech/nate/iteneraysystem/TaskAdapter.java
@@ -0,0 +1,35 @@
+package com.tech.nate.iteneraysystem;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public TaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
