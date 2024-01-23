1503babdf6fde9c360807b96e3e94b9b025a5838
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-23 02:21:33 +0000

    updated

---
+++ app/src/main/java/com/tech/nate/iteneraysystem/AllTasks.java
@@ -0,0 +1,14 @@
+package com.tech.nate.iteneraysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks);
+    }
+}
