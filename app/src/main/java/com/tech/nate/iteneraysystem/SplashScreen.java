757e69899f99fb0eb7330696924189e940a9dac7
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-23 04:25:03 +0000

    updated

--- app/src/main/java/com/tech/nate/iteneraysystem/SplashScreen.java
+++ app/src/main/java/com/tech/nate/iteneraysystem/SplashScreen.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
 
     private final int SPLASH_TIMER = 2500;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,6 +27,18 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIMER);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (null != user){
+            startActivity(new Intent(SplashScreen.this, Home.class));
+            finish();
+        }
 
     }
 }
